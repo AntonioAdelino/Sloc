0185cb30bd98b0ee86f9b104a28daa1f20277bb4
AntonioAdelino <antonio.asn03@gmail.com>
2021-10-20 23:19:52 -0300

    Filtro de rotas entre datas adicionado

--- sloc_v1/lib/TelaRotasPorVendedor.dart
+++ sloc_v1/lib/TelaRotasPorVendedor.dart
@@ -15,18 +15,12 @@ class _TelaDeRotasPorVendedor extends State<TelaDeRotasPorVendedor> {
   //////////////////////////////////////////////////////////////////
   //                          ATRIBUTOS                           //
   //////////////////////////////////////////////////////////////////
-
-  //Atributos controladores
-
-  //Atributos Form
-  final _formKey = GlobalKey<FormState>();
   bool _controlador = true;
-  String nome, email, senha, confSenha;
-
-  //Atributos
   var _rotaControlador = RotaControlador();
+  List _todasAsRotas = [];
   List _rotasBusca = [];
-  DateTime _dataSelecionada;
+  DateTime _dataInicial = DateTime.now().subtract(Duration(days: 30));
+  DateTime _dataFinal = DateTime.now();
 
   //////////////////////////////////////////////////////////////////
   //                         MÉTODOS                              //
@@ -35,52 +29,31 @@ class _TelaDeRotasPorVendedor extends State<TelaDeRotasPorVendedor> {
   _buscarRotas(Vendedor vendedor) async {
     List rotas = await _rotaControlador.listarPorVendedor(vendedor.id);
     return setState(() {
-      _rotasBusca.addAll(rotas.reversed);
+      _todasAsRotas.addAll(rotas.reversed);
     });
-  }
-
-  _quantidadeDeRotasFeitas(int vendedor) async {
-    var a = await _rotaControlador.quantidadePorVendedor(vendedor);
-    return a;
-  }
-
-  Future<List<Widget>> _pegarVisitas(int rotaId) async {
-    var visitas = await _quantidadeDeRotasFeitas(rotaId);
-    var tamanho = 1;
-    tamanho = visitas.size;
-
-    return List<Widget>.generate(
-      tamanho,
-      (i) => ListTile(
-        title: Text('Título'),
-        subtitle: Text("SUB"),
-      ),
-    );
-  }
-
-  List<Widget> _carregandoLista() {
-    return List<Widget>.generate(
-      1,
-      (i) => ListTile(
-        title: Text('Carregando...'),
-      ),
-    );
   }
 
   void _navegarParaTela(Object objeto, String rota) {
     Navigator.pushNamed(context, rota, arguments: {"objeto": objeto});
   }
 
-  _selecionarRotasPorData(DateTime dataSelecionada) {
-    var data = DateFormat("dd/MM/yyyy").format(dataSelecionada).toString();
+  _selecionarEntreDatas() {
     List rotas = [];
-    for (var item in _rotasBusca) {
-      if (item[1].contains(data)) {
+    for (var item in _todasAsRotas) {
+      var data = DateFormat('dd/MM/yyyy HH:mm').parse(item[1]);
+      if (data.isAfter(_dataInicial) && data.isBefore(_dataFinal)) {
         rotas.add(item);
       }
     }
     _rotasBusca.clear();
     _rotasBusca.addAll(rotas);
+  }
+
+  _listarTodasAsRotas() {
+    _rotasBusca.clear();
+    _rotasBusca.addAll(_todasAsRotas);
+    var a = _rotasBusca;
+    var k = 0;
   }
 
   //Inicializando o State
@@ -95,71 +68,145 @@ class _TelaDeRotasPorVendedor extends State<TelaDeRotasPorVendedor> {
 
   @override
   Widget build(BuildContext context) {
-    Map objeto = ModalRoute.of(context).settings.arguments;
+    Map objeto = ModalRoute
+        .of(context)
+        .settings
+        .arguments;
     Vendedor vendedor = objeto["objeto"];
     if (_controlador) {
       _buscarRotas(vendedor);
     }
     ;
     _controlador = false;
-
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text("Rotas"),
           backgroundColor: Color(0xff1e2e3e),
         ),
-        body: Column(children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 20, 5),
-            child: DateTimeField(
-                lastDate: DateTime.now(),
-                mode: DateTimeFieldPickerMode.date,
-                onDateSelected: (DateTime data) {
-                  _selecionarRotasPorData(data);
-                  setState(() {
-                    _dataSelecionada = data;
-                  });
-                },
-                decoration: InputDecoration(
-                  label: Text('Escolha uma data'),
-                  border: OutlineInputBorder(),
-                  suffixIcon: Icon(Icons.event_note, color: Color(0xff1e2e3e)),
-                ),
-                dateFormat: DateFormat("dd/MM/yyyy"),
-                selectedDate: _dataSelecionada),
-          ),
-          Expanded(
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: _rotasBusca.length,
-                itemBuilder: (context, index) {
-                  final rota = _rotasBusca[index];
-                  return Card(
-                    child: ListTile(
-                      title: Text("Rota: " + rota[0].toString()),
-                      subtitle: Text("Data/hora: " + rota[1]),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          GestureDetector(
-                            onTap: () {
-                              _navegarParaTela(rota[0], "/VisitasPorRotas");
-                            },
-                            child: Padding(
-                              padding: EdgeInsets.only(right: 0),
-                              child: Icon(
-                                Icons.arrow_right_alt,
-                                color: Color(0xff1e2e3e),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 5),
+                  child: Text("Escolha um período",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Color(0xff1e2e3e),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
                     ),
-                  );
-                }),
-          )
-        ]));
+                  )
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 5),
+                child: DateTimeField(
+                    lastDate: _dataFinal,
+                    mode: DateTimeFieldPickerMode.date,
+                    onDateSelected: (DateTime data) {
+                      setState(() {
+                        _dataInicial = data;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      label: Text('Data inicial'),
+                      border: OutlineInputBorder(),
+                      suffixIcon:
+                      Icon(Icons.event_note, color: Color(0xff1e2e3e)),
+                    ),
+                    dateFormat: DateFormat("dd/MM/yyyy"),
+                    selectedDate: _dataInicial),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 5),
+                child: DateTimeField(
+                    firstDate: _dataInicial,
+                    lastDate: DateTime.now(),
+                    mode: DateTimeFieldPickerMode.date,
+                    onDateSelected: (DateTime data) {
+                      setState(() {
+                        _dataFinal = data;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      label: Text('Data final'),
+                      border: OutlineInputBorder(),
+                      suffixIcon:
+                      Icon(Icons.event_note, color: Color(0xff1e2e3e)),
+                    ),
+                    dateFormat: DateFormat("dd/MM/yyyy"),
+                    selectedDate: _dataFinal),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+                Padding(padding: EdgeInsets.fromLTRB(0, 0, 10, 10),
+                  child: RaisedButton(
+                      color: Colors.grey,
+                      textColor: Colors.white,
+                      child: Text("Ver todas"),
+                      onPressed: () {
+                        setState(() {
+                          _listarTodasAsRotas();
+                        });
+                      }
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 20, 10),
+                  child: RaisedButton(
+                    color: Colors.green,
+                    textColor: Colors.white,
+                    child: Text("Buscar"),
+                    onPressed: () {
+                      setState(() {
+                        _selecionarEntreDatas();
+                      });
+                    },
+                  ),
+                )
+              ]),
+              Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
+                  child: Text("Rotas",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Color(0xff1e2e3e),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  )
+              ),
+              Expanded(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: _rotasBusca.length,
+                    itemBuilder: (context, index) {
+                      final rota = _rotasBusca[index];
+                      return Card(
+                        child: ListTile(
+                          title: Text("Rota: " + rota[0].toString()),
+                          subtitle: Text("Data/hora: " + rota[1]),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              GestureDetector(
+                                onTap: () {
+                                  _navegarParaTela(rota[0], "/VisitasPorRotas");
+                                },
+                                child: Padding(
+                                  padding: EdgeInsets.only(right: 0),
+                                  child: Icon(
+                                    Icons.arrow_right_alt,
+                                    color: Color(0xff1e2e3e),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+              )
+            ]));
   }
 }
